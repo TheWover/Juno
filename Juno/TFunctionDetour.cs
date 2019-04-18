e910150806f859f08d5656c15b337013e0c72f91
Daniel Belz <daniel_belz@gmx.net>
2019-04-19 01:29:33 +0200

    Added generic FunctionDetour class

---
+++ Juno/TFunctionDetour.cs
@@ -0,0 +1,12 @@
+﻿namespace Juno
+{
+    public class FunctionDetour<TOriginal, TDetour> : FunctionDetour
+        where TOriginal : class
+        where TDetour : class
+    {
+        public FunctionDetour(string originalFunctionName, string targetFunctionName) : 
+            base(typeof(TOriginal), originalFunctionName, typeof(TDetour), targetFunctionName)
+        {
+        }
+    }
+}
